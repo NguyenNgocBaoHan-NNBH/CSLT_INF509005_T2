72c6b1c0759da52ce715a785feeab12cb6602c0b
NguyenNgocBaoHan-NNBH <nguyenngocbaohan265@gmail.com>
2026-08-18 15:37:32 +0700

    Add project files.

---
+++ ConsoleApp1/session01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Xin chào");
+        Console.WriteLine("Ciao mono");
+        Console.WriteLine("Bonjour");
+
+    }
+
+
+}
---
+++ ConsoleApp1/session02/Exercises_1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.session02
+{
+    internal class Exercises_1
+    {
+
+    }
+}
