e87782a5b1b0379a8268fb511748d949537ac8bd
NguyenNgocBaoHan-NNBH <nguyenngocbaohan265@gmail.com>
2026-08-19 11:38:51 +0700

    bai tap s2 bai 1

--- ConsoleApp1/session02/Exercises_1.cs
+++ ConsoleApp1/session02/Exercises_1.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp1.session02
             int sum = number1 + number2;
             Console.WriteLine($"{ number1} + { number2} = {sum}");
 
+            Console.WriteLine(); 
+
             //2.to Swap Values of Two Variables.
             Console.WriteLine($"Before swap number 1 = {number1}, number 2 = {number2}");
             int temp = number1;
@@ -24,10 +26,14 @@ namespace ConsoleApp1.session02
             number2 = temp;
             Console.WriteLine( $"After swap number 1 = {number1}, number 2 = {number2}");
 
+            Console.WriteLine();
+
             //3.to Multiply two Floating Point Numbers
             float f1 = 3.5f, f2 = 2.7f;
             float f3 = f1 * f2;
             Console.WriteLine( $"(f1) * (f2) = {f3}");
+
+            Console.WriteLine();
 
             //4.to convert feet to meter
             float feet = 5.7f;
@@ -35,31 +41,45 @@ namespace ConsoleApp1.session02
             float metter = rate * feet;
             Console.WriteLine( $"{feet}feet = {metter} metter.");
 
+            Console.WriteLine();
+
             //5.to convert Celsius to Fahrenheit and vice versa
             float cels = 27f;
             float fah = cels * 1.8f + 32;
             Console.WriteLine( $"{cels}°C = {fah}°F");
 
+            Console.WriteLine();
+
             //6.to find the Size of data types
             Console.WriteLine( $"Size of double data type is {sizeof(double)}");
             Console.WriteLine($"Size of int data type is {sizeof(int)}");
 
+            Console.WriteLine();
+
             //7.to Print ASCII Value(tip: read character, print number of this char)
             Console.Write("Enter a character: ");
             int c = Console.Read();
+            Console.ReadLine();
             Console.WriteLine($"ASCII code of {(char)c} is {c}");
+
+            Console.WriteLine();
 
             //8.to Calculate Area of Circle
             Console.Write("Enter radius: ");
             double r = Convert.ToDouble(Console.ReadLine());
             double circleArea = 3.14 * r * r;
-            Console.WriteLine("Area of Circle is {circleArea}");
+            Console.WriteLine($"Area of Circle is {circleArea}");
+
+            Console.WriteLine();
 
             //9.to Calculate Area of Square
             Console.Write("Enter side of square: ");
             double side = Convert.ToDouble(Console.ReadLine());
             double squareArea = side * side;
             Console.WriteLine($"Area of Square is: {squareArea}");
+
+            Console.WriteLine();
+
             //10.to convert days to years, weeks and days
             Console.Write("Enter total days: ");
             int totalDays = Convert.ToInt32(Console.ReadLine());
@@ -68,6 +88,7 @@ namespace ConsoleApp1.session02
             int days = (totalDays % 365) % 7;
             Console.WriteLine($"{totalDays} days = {years} years, {weeks} weeks, {days} days");
 
+            Console.WriteLine(); 
 
             Console.WriteLine( "Press any key to continue...");
             Console.ReadKey();
